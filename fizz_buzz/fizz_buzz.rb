c819d33caa219df84528158cc972513ea71ab7a1
sato akihide <aki@sato.local>
2019-10-09 01:43:10 +0900

    Add: fizz_buzz

---
+++ fizz_buzz/fizz_buzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz(n)
+  if n % 15 == 0
+    p 'fizzbuzz'
+  elsif n % 5 == 0
+    p 'buzz'
+  elsif n % 3 == 0
+    p 'fizz'
+  else 
+    p n.to_s
+  end
+end
+
+for i in 1..100 do
+  fizz_buzz(i)
+end
