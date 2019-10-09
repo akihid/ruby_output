a2668d831222901eb00df262f06c0c64fc1accb6
sato akihide <[akihide315@gmail.com]>
2019-10-09 17:00:23 +0900

    Add: fizzbuzz(rspecによるテスト実装)

--- fizz_buzz/spec/fizz_buzz_spec.rb
+++ fizz_buzz/spec/fizz_buzz_spec.rb
@@ -7,4 +7,22 @@ describe 'fizz_buzz' do
       expect(fizz_buzz(6)).to eq('fizz')
     end
   end
+  context 'buzzが出力されるケース' do
+    it '5の倍数ではbuzzが表示される' do
+      expect(fizz_buzz(5)).to eq('buzz')
+      expect(fizz_buzz(10)).to eq('buzz')
+    end
+  end
+  context 'fizzbuzzが出力されるケース' do
+    it '15の倍数ではfizzbuzzが表示される' do
+      expect(fizz_buzz(15)).to eq('fizzbuzz')
+      expect(fizz_buzz(30)).to eq('fizzbuzz')
+    end
+  end
+  context '数字が出力されるケース' do
+    it '3の倍数でも5の倍数でもない場合数字が表示される' do
+      expect(fizz_buzz(2)).to eq('2')
+      expect(fizz_buzz(7)).to eq('7')
+    end
+  end
 end
