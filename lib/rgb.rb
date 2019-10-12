a413e1be321296c19cf1d4e116db6e6008075b71
sato akihide <akihide315@gmail.com>
2019-10-12 16:48:06 +0900

    Add: to_initsメソッドテスト

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -9,3 +9,15 @@ def to_hex(r, g, b)
     hex += n.to_s(16).rjust(2, '0')
   end
 end
+
+def to_inits(hex)
+  # r = hex[1..2]
+  # g = hex[3..4]
+  # b = hex[5..6]
+  # r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  # r, g, b = hex.scan(/\w\w/)
+  # [r, g, b].map do |s|
+  #   s.hex
+  # end
+  hex.scan(/\w\w/).map(&:hex)
+end
--- spec/rgb_spec.rb
+++ spec/rgb_spec.rb
@@ -8,5 +8,11 @@ describe 'rgb' do
       expect(to_hex(4, 60, 120)).to eq('#043c78')
     end
   end
-
+  context '16進数を10進数に変換するケース' do
+    it 'to_initsメソッド' do
+      expect(to_inits('#000000')).to eq([0, 0, 0])
+      expect(to_inits('#ffffff')).to eq([255, 255, 255])
+      expect(to_inits('#043c78')).to eq([4, 60, 120])
+    end
+  end
 end
