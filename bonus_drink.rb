272566c4cea7578c17c0f7bf9434af1bbcfdde27
sato akihide <akihide315@gmail.com>
2019-10-12 19:21:32 +0900

    Add: ボーナスドリンク問題

---
+++ bonus_drink.rb
@@ -0,0 +1,38 @@
+class BonusDrink
+
+  def initialize(drinks)
+    @drinks = drinks  # ドリンクの数
+    @drank_count = 0  # 飲んだドリンクの数
+    @empty_count = 0  # 空の缶の数
+    @bonus_count = 0  # 追加で飲める数
+  end
+
+  def self.total_count_for(amount)
+    BonusDrink.new(amount).drink_count
+  end
+
+  def drink_count
+    until @drinks.zero?
+      drink
+      bonus_exchange
+      puts '---'
+    end
+    @drank_count
+  end
+
+  private
+
+  def drink
+    @drank_count += @drinks
+    @empty_count += @drinks
+    @drinks = 0
+    puts "drink #{@drank_count}"
+  end
+
+  def bonus_exchange
+    @bonus_count, @empty_count = @empty_count.divmod(3)
+    @drinks += @bonus_count
+    puts "bonus #{@bonus_count}"
+  end
+
+end
